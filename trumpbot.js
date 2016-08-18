99901ea56b25ba5d2ad5edc871b3cc02ecc8865d
ustcboulder <huidai.ustc@gmail.com>
2016-08-18 12:33:46 -0700

    Tutorial code on how to create a simple slack bot

---
+++ trumpbot.js
@@ -0,0 +1,39 @@
+var SlackBot = require('slackbots');
+var bot = new SlackBot({
+    token: 'xoxb-70734356564-7UaHzs2p0enqPhSGPYw5EqeK', // put the token
+    name: 'trumpbot'
+});
+
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('quotes.txt')
+ });
+
+var quotes = []; 
+rl.on('line', function (line) {
+    quotes.push(line);
+});
+
+
+bot.on('start', function() {
+    for(var i=0; i<this.users.length; i++){
+        if(this.users[i].name == bot.name){
+            this.user = this.users[i];
+            break;
+        }
+    }
+});
+
+
+bot.on('message', function(data) {
+
+    if(data.type === 'message' && data.text){
+        if(data.user !== this.user.id){
+            var randomInt = Math.floor(Math.random() * (quotes.length));
+            bot.postMessage(data.channel, quotes[randomInt], {as_user: true});
+        }
+    } 
+   
+})
